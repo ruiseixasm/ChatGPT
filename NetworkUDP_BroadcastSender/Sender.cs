5b7f834406c65a4e0cb1d13aefdc85bebb0017f8
ruiseixasm <ruiseixas@yahoo.com>
2023-03-29 23:14:43 +0100

    Last Updates!

--- NetworkUDP_BroadcastSender/Sender.cs
+++ NetworkUDP_BroadcastSender/Sender.cs
@@ -11,23 +11,33 @@ namespace NetworkUDP_BroadcastSender
         {
             // Set the broadcast address and port
             IPAddress broadcastAddress = IPAddress.Parse("192.168.31.255");
-            int port = 11000;
+            int port = 3108;
 
             // Create the socket
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
             // Enable broadcast option
             socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            while (true)
+            {
+                // Construct the message as a byte array
+                Console.Write("Enter message: ");
+                string message = Console.ReadLine();
 
-            // Construct the message as a byte array
-            string message = "Hello, World!";
-            byte[] buffer = Encoding.ASCII.GetBytes(message);
+                if (!string.IsNullOrEmpty(message))
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes(message);
+                    // Create the endpoint for broadcasting to the specified address and port
+                    IPEndPoint endPoint = new IPEndPoint(broadcastAddress, port);
 
-            // Create the endpoint for broadcasting to the specified address and port
-            IPEndPoint endPoint = new IPEndPoint(broadcastAddress, port);
-
-            // Broadcast the message
-            socket.SendTo(buffer, endPoint);
+                    // Broadcast the message
+                    socket.SendTo(buffer, endPoint);
+                }
+                else
+                {
+                    break;
+                }
+            }
 
             // Close the socket
             socket.Close();
