2204d319c979662ba77d0236618a25e31ce87091
Rui <ruiseixas@yahoo.com>
2023-03-29 05:16:58 +0100

    Network UDP Broadcast

---
+++ NetworkUDP_BroadcastReceiver/Receiver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkUDP_BroadcastReceiver
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            const int listenPort = 11000;
+
+            UdpClient listener = new UdpClient(listenPort);
+            Console.WriteLine($"Listening for broadcast messages on port {listenPort}...");
+
+            while (true)
+            {
+                try
+                {
+                    // Wait for a broadcast message
+                    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    // Display the message
+                    string message = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                    Console.WriteLine($"Received broadcast message from {groupEP}: {message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error receiving broadcast message: {e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ NetworkUDP_BroadcastSender/Sender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkUDP_BroadcastSender
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            // Set the broadcast address and port
+            IPAddress broadcastAddress = IPAddress.Parse("192.168.31.255");
+            int port = 11000;
+
+            // Create the socket
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // Enable broadcast option
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            // Construct the message as a byte array
+            string message = "Hello, World!";
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+            // Create the endpoint for broadcasting to the specified address and port
+            IPEndPoint endPoint = new IPEndPoint(broadcastAddress, port);
+
+            // Broadcast the message
+            socket.SendTo(buffer, endPoint);
+
+            // Close the socket
+            socket.Close();
+
+            Console.WriteLine("Message broadcasted.");
+        }
+    }
+}
