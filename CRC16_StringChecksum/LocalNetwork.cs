6b14ad682bc17edeebe4ee73ecbeb2dec1055340
ruiseixasm <ruiseixas@yahoo.com>
2023-03-30 16:14:47 +0100

    New Changes

--- CRC16_StringChecksum/LocalNetwork.cs
+++ CRC16_StringChecksum/LocalNetwork.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Collections.Generic;
 
-namespace NetworkInterface
+namespace NetworkCommunication
 {
     public class LocalNetwork
     {
         private static int communicationPort = 3108;
-        private static UdpClient listener = new UdpClient(communicationPort);;
+        private static UdpClient listener = new UdpClient(communicationPort);
         private static Socket socketBroadcast = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Create the socket
         private static Dictionary<string, IPAddress> localNetworkAddresses = new Dictionary<string, IPAddress>();
 
@@ -18,24 +19,77 @@ namespace NetworkInterface
             socketBroadcast.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
         }
 
-        public bool SendText(string[] textString)
+        public bool SendString(string[] sendString)
         {
+            if (sendString.Length == 1)
+            {
+                if (sendString[0] == "recall")
+                {
+                    RecallAddresses();
+                    return true;
+                }
+            }
+            else
+            {
+                var message = new Tuple<string, IPAddress>(sendString[0], localNetworkAddresses[sendString[1]]);
+                SendMessage(message);
+                return true;
+            }
             return false;
         }
 
-        public string[] ReadText()
+        public string[] ReceiveString()
         {
-            string[] result = new string[2];
+            Tuple<string, IPAddress> message = ReceiveMessage();
 
+            if (message != null)
+            {
 
+                string[] words = message.Item1.Split(' ');
 
-            return result;
+                if (words.Length == 1) // just the name means new regist
+                {
+                    localNetworkAddresses[words[0]] = message.Item2;
+                }
+
+                string[] result = { message.Item1, words[0] };
+                return result;
+            }
+
+            return null;
         }
 
-        public int RefreshAddresses()
+        public void RecallAddresses()
         {
+            List<IPAddress> broadcastAddresses = new List<IPAddress>();
 
-            return 0;
+            // Get the local IP address and subnet mask
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            IPAddress subnetMask = IPAddress.None;
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (adapter.OperationalStatus == OperationalStatus.Up && adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    foreach (UnicastIPAddressInformation local_ip in adapter.GetIPProperties().UnicastAddresses)
+                    {
+                        if (local_ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            subnetMask = local_ip.IPv4Mask;
+                            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+                            if (subnetMaskBytes[1] == 255 || true) // Avoids 127.0.0.1 addresses!
+                            {
+                                IPAddress broadcastAddress = BroadcastAddress(local_ip.Address, subnetMask);
+                                if (!broadcastAddresses.Contains(broadcastAddress))
+                                {
+                                    var message = new Tuple<string, IPAddress>("Recall", broadcastAddress);
+                                    SendMessage(message);
+                                    broadcastAddresses.Add(broadcastAddress);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private Tuple<string, IPAddress> ReceiveMessage()
@@ -48,20 +102,6 @@ namespace NetworkInterface
                 string message = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
 
                 return Tuple.Create(message, groupEP.Address);
-
-                if (message == "Hello")
-                {
-                    if (localNetworkAddresses.ContainsValue(groupEP.Address))
-                    {
-
-                    }
-                else
-                {
-
-                }
-
-                // Display the message
-                Console.WriteLine($"Received broadcast message from {groupEP}: {message}");
             }
             catch (Exception e)
             {
@@ -75,7 +115,26 @@ namespace NetworkInterface
             byte[] buffer = Encoding.ASCII.GetBytes(message.Item1);
             // Create the endpoint for broadcasting to the specified address and port
             IPEndPoint endPoint = new IPEndPoint(message.Item2, communicationPort);
-            socketBroadcast.SendTo(buffer, endPoint); // Broadcast the message
+            socketBroadcast.SendTo(buffer, endPoint); // Broadcast or Unicast the message
+        }
+
+        private static IPAddress BroadcastAddress(IPAddress localAddress, IPAddress subnetMask)
+        {
+            byte[] localAddressBytes = localAddress.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+            Array.Reverse(localAddressBytes);
+            Array.Reverse(subnetMaskBytes);
+
+            UInt32 localAddressInt = BitConverter.ToUInt32(localAddressBytes, 0);
+            UInt32 subnetMaskInt = BitConverter.ToUInt32(subnetMaskBytes, 0);
+
+            UInt32 first_address = localAddressInt & subnetMaskInt; // Network Address
+            UInt32 last_address = localAddressInt | ~subnetMaskInt; // Broadcast Address
+
+            byte[] broadcastAddressBytes = BitConverter.GetBytes(last_address);
+            Array.Reverse(broadcastAddressBytes);
+
+            return new IPAddress(broadcastAddressBytes);
         }
 
         private static ushort TextCRC16(string textString)
