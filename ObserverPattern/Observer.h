1145eb58d279edd0fbfd5df5e24b26b554021297
Rui <ruiseixas@yahoo.com>
2023-04-05 22:01:39 +0100

    Updated Code

--- ObserverPattern/Observer.h
+++ ObserverPattern/Observer.h
@@ -3,10 +3,15 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include "Subject.h"
+
 class Observer {
 public:
-    virtual ~Observer() {}
+    Observer(Subject* subject);
+    virtual ~Observer();
     virtual void update() = 0;
+protected:
+    Subject* subject_;
 };
 
 #endif // OBSERVER_H
